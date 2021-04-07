ef1f9ac3cad967176502d2c82aacd618fb0985c8
daulen <daulenz@gmail.com>
2021-04-07 17:09:56 -0500

    commit tree 7c9766336595324e57379384d9d455554351c1ce

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks){
+   
+    return  Math.abs(blocks - 42)
+}
+
+function distanceFromHqInFeet(blocks){
+
+    return  Math.abs(blocks - 42)*264
+}
+
+function distanceTravelledInFeet(block1,block2){
+    return Math.abs(block1-block2)*264
+}
+
+function calculatesFarePrice(start,destination){
+    let price, distance;
+    distance = Math.abs(start-destination)*264;
+
+if (distance < 400){
+    return price = 0;
+    } else if (distance >400 && distance < 2000){
+    return price = (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500){
+    return price = 25;
+    }
+    else{
+      return  price ='cannot travel that far'  
+    }
+
+}
+
